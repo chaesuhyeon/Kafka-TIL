63a486754884102e9f48c2b0e77c4b3ae57ef496
suhyeon chae <ddd8177@gmail.com>
2024-01-25 21:47:06 +0900

    내장 카프카를 사용한 카프카 통합 테스트

--- udemy/library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ udemy/library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -27,9 +27,9 @@ public class LibraryEventsController {
         log.info("libraryEvent: {}", libraryEvent);
         // invoke the kafka producer
 //        libraryEventsProducer.sendLibraryEvent(libraryEvent);
-//        libraryEventsProducer.sendLibraryEvent_approach2(libraryEvent);
+        libraryEventsProducer.sendLibraryEvent_approach2(libraryEvent);
 
-        libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
+//        libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
         log.info("After Sending libraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
