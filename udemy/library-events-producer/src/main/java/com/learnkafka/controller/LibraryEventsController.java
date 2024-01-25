fe022d4fb11976428d28585d9a07c67a9473757b
suhyeon chae <ddd8177@gmail.com>
2024-01-25 22:17:44 +0900

    내장 카프카를 사용한 카프카 단위 테스트

--- udemy/library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ udemy/library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -3,6 +3,7 @@ package com.learnkafka.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
 import com.learnkafka.producer.LibraryEventsProducer;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -22,14 +23,13 @@ public class LibraryEventsController {
     private final LibraryEventsProducer libraryEventsProducer;
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
 
         log.info("libraryEvent: {}", libraryEvent);
         // invoke the kafka producer
 //        libraryEventsProducer.sendLibraryEvent(libraryEvent);
-        libraryEventsProducer.sendLibraryEvent_approach2(libraryEvent);
-
-//        libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
+//        libraryEventsProducer.sendLibraryEvent_approach2(libraryEvent);
+        libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
         log.info("After Sending libraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
